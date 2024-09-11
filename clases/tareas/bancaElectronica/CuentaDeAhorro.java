1eef345597ec61859e5bd44eeb4c7985abba4f15
lfrobayo <luisarob21@gmail.com>
2024-09-11 17:36:25 -0500

    Exercise banca electronica firts part

---
+++ clases/tareas/bancaElectronica/CuentaDeAhorro.java
@@ -0,0 +1,31 @@
+package tareas.bancaElectronica;
+
+public class CuentaDeAhorro extends Cuenta{
+
+    private double tasaInteresMenual;
+
+    public CuentaDeAhorro(int numero, double saldo, double tasaInteresMenual) {
+        super(numero, saldo);
+        this.tasaInteresMenual=tasaInteresMenual;
+    }
+
+    public double getTasaInteresMenual() {
+        return tasaInteresMenual;
+    }
+
+    public void setTasaInteresMenual(double tasaInteresMenual) {
+        this.tasaInteresMenual = tasaInteresMenual;
+    }
+
+    public double calcularIntereses(){
+        double interes= getSaldo()*(getTasaInteresMenual()/100);
+        return interes;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaDeAhorro{" +
+                "tasaInteresMenual=" + tasaInteresMenual +
+                '}';
+    }
+}
---
+++ clases/tareas/bancaElectronica/CuentaDeCheque.java
@@ -0,0 +1,18 @@
+package tareas.bancaElectronica;
+
+public class CuentaDeCheque extends Cuenta{
+
+    private double costoManejoMensual;
+
+    public CuentaDeCheque(int numero, double saldo, double costoManejoMensual) {
+        super(numero, saldo);
+        this.costoManejoMensual=costoManejoMensual;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaDeCheque{" +
+                "costoManejoMensual=" + costoManejoMensual +
+                '}';
+    }
+}
