360c6b739bcdb9aeea5772b4cb81178bafa7fab2
lfrobayo <luisarob21@gmail.com>
2024-09-10 20:33:38 -0500

    Practice Java Beginner

---
+++ clases/practicas/runner.java
@@ -0,0 +1,13 @@
+package practicas;
+
+public class runner {
+    public static void main(String[] args) {
+        EjerciciosFor ejercicios= new EjerciciosFor();
+        //ejercicios.Descendente(1,10);
+       //ejercicios.Pares(20);
+        //ejercicios.Impares(20);
+        //ejercicios.TablaCinco(10);
+        //ejercicios.Exponencial(10);
+
+    }
+}
